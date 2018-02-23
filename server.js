7049a272a001dea239d86ca56788d53a2f28f66b
madhuKR <madhukr.ramu@gmail.com>
2018-02-23 11:37:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/artical-one',function(req,res){
-    res.send("articalone will be published");
+    res.send("artical one will be published");
 });
 app.get('/ui/artical-two',function(req,res){
     res.send("artical two will be published");
