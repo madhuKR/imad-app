7df8224f7df2a0585aa5679e56cb87c5e06e2dd6
madhuKR <madhukr.ramu@gmail.com>
2018-02-27 10:24:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/article-one',function(req,res){
+app.get('/article-one',function(req,res){
     //articelName=articel-one
     //articels[articelName]={} content of artical one
     //var articelName = req.params.articelName;
