238714c0ae127d171db15e2af3e2478d5893e51d
madhuKR <madhukr.ramu@gmail.com>
2018-02-27 18:50:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ consol.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML= "New valu";
 var img = document.getElementById('madi');
-img.onlick=function()
+img.onlick = function()
 {
-    img.style.marginlLeft='110px';
+    img.style.marginlLeft = '110px';
 };
